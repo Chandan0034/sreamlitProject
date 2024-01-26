8e4f5ccf65e641bfd286431f1583f3eabc8619bd
CHANDAN KUMAR <125118924+Chandan0034@users.noreply.github.com>
2024-01-26 22:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from pytube import YouTube
+import os
+import platform
+
 def download_video(video_url):
     try:
         yt = YouTube(video_url)
         video_stream = yt.streams.get_highest_resolution()
-        video_stream.download()
+        location=os.path.join(os.path.expanduser("~"), "Downloads")
+        video_stream.download(output_path=location)
         st.success("Download complete.")
+        
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
 def main():
     st.title("YouTube Video Downloader")
+
     # Get YouTube video URL from user input
     video_url = st.text_input("Enter YouTube Video URL:")
     
@@ -21,4 +27,5 @@ def main():
             st.warning("Please enter a valid YouTube video URL.")
 
 if __name__ == "__main__":
+    st.text(f"Operation System  {platform.system()}")
     main()
